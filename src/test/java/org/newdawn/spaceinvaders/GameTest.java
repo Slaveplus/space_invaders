5fd15d6dda2752e295d0988326e0019aacf6f90a
neuroner0425 <neuroner0425@gmail.com>
2025-09-17 15:42:32 +0900

    feat: add game source files

--- src/test/java/org/newdawn/spaceinvaders/GameTest.java
+++ src/test/java/org/newdawn/spaceinvaders/GameTest.java
@@ -1,4 +1,11 @@
 package org.newdawn.spaceinvaders;
 
+import org.junit.Test;
+
 public class GameTest {
+    @Test
+    public void testMain(){
+        Game g = new Game();
+        g.gameLoop();
+    }
 }
